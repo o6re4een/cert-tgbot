be3cf0bfe294d3ef6996ae1beedf7ca27f92dc84
artem <artem.portnov2014@gmail.com>
2024-06-16 23:44:40 +0500

    bot fix

--- cert_builder.py
+++ cert_builder.py
@@ -1,38 +1,70 @@
 # import PyPDF2
 import os
-# from fpdf import FPDF
+from fpdf import FPDF
 import fitz  
 import datetime
 from fitz import *
 
 
-def create_certificate(name:str,title:str,date:str, template_path:str, output_path:str):
+
+
+def image_to_pdf(image_path:str) -> str:
+    output_path = os.path.splitext(image_path)[0] + ".pdf"
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.image(image_path, x=0, y=0, w=210, h=297)
+    path = pdf.output(output_path)
+    return output_path
+
+def create_custom_cert(custom_template_path:str="", output_path:str = "", custom_search_text:str="", custom_data_text:str=""):
+    pdf_path = image_to_pdf(custom_template_path)
+    doc = fitz.open(pdf_path)
+    if (len(custom_data_text.split(';')) == len(custom_search_text.split(';'))):
+        for i in range(0, len(custom_search_text.split(';'))):
+            search_text = custom_search_text.split(';')[i]
+            replace_text = custom_data_text.split(';')[i]
+            for page in doc:
+                found_item: list[Shape]= page.search_for(search_text)
+                if(not found_item):
+                    print('not found', search_text)
+                    break
+                page.add_redact_annot(found_item[0], '')  # create redaction for text
+                page.apply_redactions()  # apply the redaction now
+                page.insert_textbox(found_item[0] , replace_text, fontsize=20, fontname="Helvetica", encoding=TEXT_ENCODING_LATIN, lineheight=0.8, align=TEXT_ALIGN_CENTER)
+                # page.save(output_path)
+    doc.save(output_path)
+    return True
+
+
+def create_certificate(name:str = "",title:str = "",date:str = "", template_path:str = "", output_path:str = ""):
     # Открываем шаблонный PDF
-    doc = fitz.open(template_path)
    
     
-    search_text = "{{name}}"
+    
+   
+    doc = fitz.open(template_path)
     for page in doc:
+        search_text = "{{name}}"
         found_name: list[Shape]= page.search_for(search_text)
         found_date: list[Shape] = page.search_for('{{date}}')
         found_title : list[Shape] = page.search_for('{{title}}')
 
         if(not found_name):
             return False
-        # print(found_item)
+            # print(found_item)
 
-        # page.add_redact_annot(found_item[0], '')  # create redaction for text
-        # page.apply_redactions()  # apply the redaction now
-        # page.insert_text(found_item[0].bl , name, fontsize=20, fontname="Helvetica", encoding="UTF-8")
-        
-        # page.add_redact_annot(found_date[0], '')  # create redaction for text
-        # page.apply_redactions()  # apply the redaction now
-        # page.insert_text(found_date[0].bl , datetime.date.today().strftime("%d.%m.%Y"), fontsize=12, fontname="Helvetica", encoding="UTF-8")
-        
+            # page.add_redact_annot(found_item[0], '')  # create redaction for text
+            # page.apply_redactions()  # apply the redaction now
+            # page.insert_text(found_item[0].bl , name, fontsize=20, fontname="Helvetica", encoding="UTF-8")
+            
+            # page.add_redact_annot(found_date[0], '')  # create redaction for text
+            # page.apply_redactions()  # apply the redaction now
+            # page.insert_text(found_date[0].bl , datetime.date.today().strftime("%d.%m.%Y"), fontsize=12, fontname="Helvetica", encoding="UTF-8")
+            
         page.add_redact_annot(found_name[0], '')  
         page.apply_redactions()  
         page.insert_textbox(found_name[0] , name, fontsize=20, fontname="Helvetica", encoding=TEXT_ENCODING_LATIN, lineheight=0.8, align=TEXT_ALIGN_CENTER)
-        
+            
         page.add_redact_annot(found_title[0], '')  
         page.apply_redactions()  
         page.insert_textbox(found_title[0] , title, fontsize=20, fontname="Helvetica", encoding=TEXT_ENCODING_LATIN, lineheight=0.8, align=TEXT_ALIGN_CENTER, expandtabs=8)
--- config.py
+++ config.py
@@ -11,6 +11,11 @@ class States(Enum):
     S_ENTER_DATE = "3" #Дата проведения
     S_ENTER_CONG = "4" #Успех
     S_SEND_DOCS = "5" #Отправка файла
+    
+    S_CUSTOM_CERT_WAIT_FOR_TEMPLATE = "7" #Ждем шаблон
+    S_CUSTOM_CERT_WAIT_FOR_TEMPLATE_STR="8" #Ждем шаблоны строк для поиска
+    S_CUSTOM_CERT_WAIT_FOR_REPLACEMENT_STR = "9" #Ждем шаблоны строк для замены
+    S_CUSTOM_CERT_WAIT_FOR_GENERATION = "10" #Создаем сертификат 
 
 
 # class User():
